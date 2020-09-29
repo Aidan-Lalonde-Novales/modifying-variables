be236bb0616a86c9b3dbe3e0f55bfd55c3a7d1b6
Aidan Lalonde-Novales <71395339+Aidan-Lalonde-Novales@users.noreply.github.com>
2020-09-28 20:55:22 -0400

    Added two comments.

--- main.ts
+++ main.ts
@@ -1,4 +1,14 @@
+// Created By: Aidan L-N
+// Created Sept 28 2020
+// 
+// Sets hungyness to zero on start.
 let hungryness = 0
+// Created By: Aidan L-N
+// Created Sept 28 2020
+// 
+// Increases hungyness by 1 on A press.
+// Sets hungyness to zero on B press.
+// 
 basic.forever(function () {
     if (input.buttonIsPressed(Button.A)) {
         hungryness = hungryness + 1
